412bddf9ade7c15f0e4b874ff700b20970573820
sopoour <sophia.auer@googlemail.com>
2021-12-03 18:55:19 +0100

    removed some legacy code

--- src/App.js
+++ src/App.js
@@ -7,28 +7,6 @@ import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
 
 function App() {
-  const [quotes, setQuotes] = useState([
-    {
-      id: "q1",
-      author: "Sophia",
-      text: "this is a quote by sophia",
-    },
-    {
-      id: "q2",
-      author: "Anna",
-      text: "this is a quote by Anna",
-    },
-  ]);
-
-  const handleAddQuote = (newQuote) => {
-    console.log("In app:" + newQuote);
-    setQuotes((prevQuotes) => {
-      return [newQuote, ...prevQuotes];
-    });
-  };
-
-  console.log(quotes);
-
   return (
     <Layout>
       <Switch>
@@ -39,10 +17,10 @@ function App() {
           <AllQuotes />
         </Route>
         <Route path="/quotes/:quoteId">
-          <QuoteDetail quotes={quotes} />
+          <QuoteDetail />
         </Route>
         <Route path="/new-quote">
-          <AddQuote onAddQuote={handleAddQuote} />
+          <AddQuote />
         </Route>
         {/* To add a catch-all/404/not found page just put the path to the star and at the total end of the Switch */}
         <Route path="*">
